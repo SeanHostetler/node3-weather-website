21ad9fbe222b23d76ef98741407a0c01cd88af60
Hostetler <sean.hostetler@beckshybrids.com>
2020-11-10 14:42:03 -0500

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('postman-request')
 
+
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=4762a0f44a9d230c68411c2f8022fc3e&query='+ latitude + ',' + longitude + '&units=f'
 
@@ -9,7 +11,10 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. The temperature is '+ body.current.temperature+ ' degrees.  It feels like '+ body.current.feelslike + ' degrees.')
+            callback(
+                undefined, body.current.weather_descriptions[0] + '. The temperature is '
+                + body.current.temperature+ ' degrees.  It feels like '+ body.current.feelslike + ' degrees.  The humidity is ' 
+                + body.current.humidity + '%.')
         }
     })
 }
