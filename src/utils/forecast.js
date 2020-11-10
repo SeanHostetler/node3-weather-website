8a7dd70011d90d7be0aefe8853f16eea6ef02f90
Hostetler <sean.hostetler@beckshybrids.com>
2020-11-10 13:20:06 -0500

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('postman-request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=4762a0f44a9d230c68411c2f8022fc3e&query='+ latitude + ',' + longitude + '&units=f'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. The temperature is '+ body.current.temperature+ ' degrees.  It feels like '+ body.current.feelslike + ' degrees.')
+        }
+    })
+}
+
+
+module.exports = forecast
